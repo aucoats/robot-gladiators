10177c79b0070b52bf7ff7482e2847f830c5b149
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-10-20 14:38:05 -0400

    numeric values for shop options

--- assets/js/game.js
+++ assets/js/game.js
@@ -190,7 +190,6 @@ var startGame = function() {
         if (playerInfo.health > 0) {
             // let player know what round, arrays start at 0 so i + 1
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
-            debugger;
 
             // pick new enemy to fight based on index of enemy.names array
             var pickedEnemyObj = enemyInfo[i];
@@ -247,21 +246,19 @@ var endGame = function() {
 var shop = function() {
     // ask what do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', OR 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for refill, 2 for upgrade, or 3 for leave to make a choice."
     );
-
+    
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             //do nothing, so function will end
